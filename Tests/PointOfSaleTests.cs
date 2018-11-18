4578be34a1b6fa0e4e5e6702bd9e479bba296d26
John Welty <john@weltyautomation.com>
2018-11-18 03:23:49 -0600

    Display greeting message on startup.

---
+++ Tests/PointOfSaleTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using FluentAssertions;
+
+namespace Tests
+{
+    public class PointOfSaleTests
+    {
+        [Fact]
+        public void StartsWithGreeting()
+        {
+            DisplaySpy display = new DisplaySpy();
+            var pos = new PointOfSaleTerminal(display);
+
+            display.Displayed.Should().Be("Welcome!");
+        }
+    }
+
+    public interface IDisplay
+    {
+        void Display(string message);
+    }
+
+    public class DisplaySpy : IDisplay
+    {
+        public void Display(string message)
+        {
+            Displayed = message;
+        }
+
+        public string Displayed { get; private set; }
+    }
+
+    public class PointOfSaleTerminal
+    {
+        public PointOfSaleTerminal(IDisplay display)
+        {
+            display.Display("Welcome!");
+        }
+    }
+}
