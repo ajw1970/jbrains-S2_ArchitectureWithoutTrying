9766110fba8da8d66be8cb5e67ffcb50f391d1f8
John Welty <john@weltyautomation.com>
2018-11-18 03:05:35 -0600

    Add project files.

---
+++ PointOfSaleTests/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using FluentAssertions;
+
+namespace PointOfSaleTests
+{
+    public class Class1
+    {
+        [Fact]
+        public void Nothing()
+        {
+
+        }
+    }
+}
