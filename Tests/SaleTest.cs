ce3d2d22f1055158ffd5a5003e846ef62e77971c
John Welty <john@weltyautomation.com>
2018-11-26 04:27:00 -0600

    Reset project for starting over with Whole Value Objects.

---
+++ Tests/SaleTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using FluentAssertions;
+
+namespace Tests
+{
+    public class SaleTest
+    {
+        [Fact]
+        public void Nothing()
+        {
+            false.Should().BeTrue();
+        }
+    }
+}
