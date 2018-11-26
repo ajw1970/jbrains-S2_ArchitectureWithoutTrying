48fa551b5b7b93a39db01b905a2b17e56e473b1a
John Welty <john@weltyautomation.com>
2018-11-26 04:52:39 -0600

    We can sell any item as long as we charge $7.95

--- Tests/SaleTest.cs
+++ Tests/SaleTest.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Net.Mime;
 using System.Text;
 using Xunit;
 using FluentAssertions;
@@ -10,9 +12,35 @@ namespace Tests
     public class SellOneItemTest
     {
         [Fact]
-        public void Nothing()
+        public void ProductFound()
         {
-            false.Should().BeTrue();
+            var display = new Display();
+            var sale = new Sale(display);
+            sale.OnBarcode(new Barcode("12345"));
+            display.GetText().Should().Be("$7.95");
+        }
+
+        public class Barcode
+        {
+            public Barcode(string value)
+            {
+            }
+        }
+
+        public class Sale
+        {
+            public Sale(Display display)
+            {
+            }
+
+            public void OnBarcode(Barcode barcode)
+            {
+            }
+        }
+
+        public class Display
+        {
+            public string GetText() => "$7.95";
         }
     }
 }
