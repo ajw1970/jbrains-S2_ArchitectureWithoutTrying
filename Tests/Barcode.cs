a5328d7d64b45912621691307432ba6a1e5467f7
John Welty <john@weltyautomation.com>
2018-11-27 12:32:18 -0600

    Add equality handling to barcode

--- Tests/Barcode.cs
+++ Tests/Barcode.cs
@@ -7,6 +7,34 @@
             Value = value;
         }
 
+        protected bool Equals(Barcode other)
+        {
+            return string.Equals(Value, other.Value);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Barcode) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return Value.GetHashCode();
+        }
+
+        public static bool operator ==(Barcode left, Barcode right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(Barcode left, Barcode right)
+        {
+            return !Equals(left, right);
+        }
+
         public string Value { get; }
     }
 }
--- Tests/BarcodeWholeValueTest.cs
+++ Tests/BarcodeWholeValueTest.cs
@@ -5,11 +5,22 @@ namespace Tests
 {
     public class BarcodeWholeValueTest
     {
-        [Fact (Skip = "Refactoring...")]
+        [Fact]
         public void CompareEqualValues()
         {
-            //var barcode1 = new Barcode("12345");
-            true.Should().BeFalse();
+            var barcode1 = new Barcode("12345");
+            var barcode2 = new Barcode("12345");
+
+            barcode1.Equals(barcode2).Should().BeTrue();
+        }
+        
+        [Fact]
+        public void CompareDifferentValues()
+        {
+            var barcode1 = new Barcode("12345");
+            var barcode2 = new Barcode("23456");
+
+            barcode1.Equals(barcode2).Should().BeFalse();
         }
     }
 }
