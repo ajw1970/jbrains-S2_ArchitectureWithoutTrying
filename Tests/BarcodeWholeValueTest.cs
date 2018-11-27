d869e9a6ba2417020cb911301806f718cfcedd2d
John Welty <john@weltyautomation.com>
2018-11-27 12:53:59 -0600

    Added a few tests for Barcode equality

--- Tests/BarcodeWholeValueTest.cs
+++ Tests/BarcodeWholeValueTest.cs
@@ -22,5 +22,32 @@ namespace Tests
 
             barcode1.Equals(barcode2).Should().BeFalse();
         }
+
+        [Fact]
+        public void CompareWithNull()
+        {
+            var barcode1 = new Barcode("12345");
+            Barcode barcode2 = null;
+
+            barcode1.Equals(barcode2).Should().BeFalse();
+        }
+
+        [Fact]
+        public void CompareEmpty()
+        {
+            var barcode1 = new Barcode("");
+            var barcode2 = new Barcode("");
+
+            barcode1.Equals(barcode2).Should().BeTrue();
+        }
+        
+        [Fact]
+        public void CompareSameInstance()
+        {
+            var barcode1 = new Barcode("sample");
+            var barcode2 = barcode1;
+
+            barcode1.Equals(barcode2).Should().BeTrue();
+        }
     }
 }
