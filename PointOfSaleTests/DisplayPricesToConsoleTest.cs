0e46bd59fa168638da951ec634ce9556de084495
John Welty <john@weltyautomation.com>
2018-12-11 04:25:45 -0600

    We can format prices expressed directly in cents

---
+++ PointOfSaleTests/DisplayPricesToConsoleTest.cs
@@ -0,0 +1,28 @@
+using FluentAssertions;
+using Xunit;
+
+namespace PointOfSaleTests
+{
+    public class DisplayPricesToConsoleTest
+    {
+        [Theory]
+        [InlineData(789, "$7.89")]
+        [InlineData(520, "$5.20")]
+        [InlineData(400, "$4.00")]
+        [InlineData(0, "$0.00")]
+        [InlineData(2, "$0.02")]
+        [InlineData(37, "$0.37")]
+        [InlineData(418976, "$4,189.76")]
+        [InlineData(210832281, "$2,108,322.81")]
+        public void Test(int priceInCents, string expectedFormattedPrice)
+        {
+            Format(priceInCents).Should().Be(expectedFormattedPrice);
+        }
+
+        private static string Format(int princeInCents)
+        {
+            decimal priceInDollars = princeInCents / 100m;
+            return $"{priceInDollars:C}";
+        }
+    }
+}
